1559589fdbd3a64067f07c7e1fea4053d114d211
imddoy <imddoy@duksung.ac.kr>
2024-06-15 20:32:56 +0900

    chore: svgr 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [
+    tsconfigPaths(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        memo: true,
+      },
+    }),
+  ],
 });
