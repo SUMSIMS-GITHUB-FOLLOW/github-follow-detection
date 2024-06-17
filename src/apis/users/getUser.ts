0407335f08e1826a6161390d9ba9fb84fd601cc1
imddoy <imddoy@duksung.ac.kr>
2024-06-17 16:16:12 +0900

    feat: 맞팔, 비맞팔 상태 저장

--- src/apis/users/getUser.ts
+++ src/apis/users/getUser.ts
@@ -1,13 +1,43 @@
+import { User } from '@typings/users/usersType';
 import instance from '../index';
 
 export const getUser = async (token: string) => {
   try {
-    const response = await instance.get(`/user`, {
+    const userResponse = await instance.get(`/user`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
-    return response.data;
+
+    const username = userResponse.data.login;
+
+    const [followersResponse, followingResponse] = await Promise.all([
+      instance.get(`/users/${username}/followers`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+      instance.get(`/users/${username}/following`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+    ]);
+
+    const followingSet = new Set(followingResponse.data.map((user: User) => user.login));
+
+    const followForFollow = followersResponse.data.filter((user: User) =>
+      followingSet.has(user.login)
+    );
+    const nonFollowForFollow = followersResponse.data.filter(
+      (user: User) => !followingSet.has(user.login)
+    );
+
+    return {
+      ...userResponse.data,
+      followForFollow,
+      nonFollowForFollow,
+    };
   } catch (error) {
     console.error('Error fetching user data:', error);
     throw error;
--- src/hooks/users/useUserStore.ts
+++ src/hooks/users/useUserStore.ts
@@ -3,5 +3,9 @@ import { create } from 'zustand';
 
 export const useUserStore = create<UserState>((set) => ({
   user: null,
+  followForFollow: [],
+  nonFollowForFollow: [],
   setUser: (user) => set({ user }),
+  setFollowForFollow: (followForFollow) => set({ followForFollow }),
+  setNonFollowForFollow: (nonFollowForFollow) => set({ nonFollowForFollow }),
 }));
